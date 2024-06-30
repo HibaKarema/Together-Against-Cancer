93510d332eb9fe147e46b22112e491f6ca4571e2
Hiba-Karema <163916154+HibaKarema@users.noreply.github.com>
2024-06-30 13:38:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+copy1.onclick = function () {
+   window.alert("Your order has been confirmed")
+ }
+ copy2.onclick = function () {
+    window.alert("Your order has been confirmed")
+ }
+ copy3.onclick = function () {
+    window.alert("Your order has been confirmed")
+ }
